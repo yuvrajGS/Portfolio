d58858b72c73cbd2eb7d4692a461fc3923fe960b
yuvrajGS <frostedwolf789@gmail.com>
2025-08-04 20:27:17 -0400

    fix import paths

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
 
 let scene, camera, renderer;
 
